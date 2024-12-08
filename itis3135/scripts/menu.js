65f3f081ae6deb5dc19e564a63f2c0492b351c01
Hafsa Konain <hkonain@uncc.edu>
2024-12-08 18:02:16 -0500

    commit changes

--- itis3135/scripts/menu.js
+++ itis3135/scripts/menu.js
@@ -65,7 +65,7 @@ document.addEventListener("DOMContentLoaded", () => {
         footerContainer.innerHTML += `
             <p>
                 Pages built by
-                <a href="hkonainstudios.com.html">HKONAIN Studio</a> © 2024
+                <a href="hkonainstudios.com.html">Humble Kangaroo Co.</a> © 2024
             </p>
             <p>
                 <a href="https://www.freecodecamp.org/certification/hkonain/responsive-web-design" target="_blank">
